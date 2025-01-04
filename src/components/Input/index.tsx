e246f8467c0af7745d21f2fa3ce4defca64cea8c
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-01-04 15:48:20 -0300

    toggleButton

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { TextInput } from 'react-native-paper';
+import React, { useState } from "react";
+import { TextInput } from "react-native-paper";
 
 interface InputProps {
   label: string;
@@ -9,26 +9,55 @@ interface InputProps {
   keyboardType?: string;
 }
 
-const Input: React.FC<InputProps> = ({ label, value, onChangeText, secureTextEntry = false, keyboardType }) => {
+const Input: React.FC<InputProps> = ({
+  label,
+  value,
+  onChangeText,
+  secureTextEntry = false,
+  keyboardType,
+}) => {
+  const [password, setPassword] = useState(secureTextEntry);
   return (
-    <TextInput
-      style={styles.container}
-      label={label}
-      value={value}
-      onChangeText={onChangeText}
-      secureTextEntry={secureTextEntry}
-      mode="outlined"
-    />
+    <>
+      <TextInput
+        style={styles.container}
+        label={label}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={password}
+        mode="outlined"
+      />
+      {secureTextEntry && (
+        <TouchableOpacity onPress={() => setPassword(!password)}>
+          <Text style={styles.toggleText}>
+            {password ? (
+              <Ionicons name="eye-off" color="#8D8D99" size={20} />
+            ) : (
+              <Ionicons name="eye" color="#8D8D99" size={20} />
+            )}
+          </Text>
+        </TouchableOpacity>
+      )}
+    </>
   );
 };
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export const styles = StyleSheet.create({
   container: {
     marginTop: 5,
     marginBottom: 5,
-  }
+  },
+  toggleText: {
+    flex: 1,
+    position: "absolute",
+    top: -45,
+    right: 15,
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+  },
 });
 
 export default Input;
